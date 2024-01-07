b4cb258d1b1cc1329f787602322bee0875d8608d
410983045 <71751207+410983045@users.noreply.github.com>
2024-01-08 00:20:13 +0800

    Update hw2.c

--- hw2.c
+++ hw2.c
@@ -13,29 +13,35 @@ int main(){
         {0, 0, 0, 0, 0, 0, 0, 0}
     };
 
+    int i, j, color;
 
-    int i,j,color;
+    scanf("%d %d %d", &i, &j, &color);
 
-    scanf("%d %d %d",&i,&j,&color);
+    if (chess[i][j] == 0) {
+        printf("空白\n");
 
+        for (int dx = -1; dx <= 1; ++dx) {
+            for (int dy = -1; dy <= 1; ++dy) {
+                if (dx == 0 && dy == 0) continue;
+                int x = i + dx;
+                int y = j + dy;
 
+                while (x >= 0 && x < 8 && y >= 0 && y < 8 && chess[x][y] == 3 - color) {
+                    x += dx;
+                    y += dy;
+                }
 
-    if (chess[i][j] == 0){
-      printf("empty");
+                if (x >= 0 && x < 8 && y >= 0 && y < 8 && chess[x][y] == color) {
+                    printf("(%d,%d) 可以下 %s 子\n", i, j, (color == 1) ? "黑" : "白");
+                    return 0;
+                }
+            }
+        }
+
+        printf("(%d,%d) 不能下 %s 子\n", i, j, (color == 1) ? "黑" : "白");
+    } else {
+        printf("(%d,%d) 已經有棋子\n", i, j);
     }
 
-    else if (chess[i][j] == 1){
-      printf("black");
-    }
-
-    else if (chess[i][j] == 2){
-      printf("white");
-    }
-
-
-
-
-
-
-   return 0;
-  }
+    return 0;
+}
